075fa6ee7d8dd0fb636e336b18db5887d35d92b3
Rasmus.Gohs@gmail.com <Rasmus.Gohs@gmail.com@ca9c9bc7-3d90-91c5-ea50-e200aa9cc870>
2010-07-23 16:20:42 +0000

    Initial commit

---
+++ sample/src/beddernet/sample/BedderTestPlatform.java
@@ -0,0 +1,75 @@
+package beddernet.sample;
+
+import android.app.Activity;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.os.Bundle;
+import android.os.IBinder;
+import android.os.RemoteException;
+import beddernet.ipc.IBEDnetService;
+import beddernet.ipc.IBEDnetServiceCallback;
+
+public class BedderTestPlatform extends Activity implements ServiceConnection {
+
+	
+	private IBEDnetService bednetService;
+	public static String applicationIdentifier = "sample application";
+	public static long applicationIdentifierHash;
+	ServiceConnection sc = this;
+
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		Intent bindIntent = new Intent("itu.bednet.approuter.BEDnetService");
+		this.bindService(bindIntent, this, Context.BIND_AUTO_CREATE);
+	}
+
+
+	public void onServiceDisconnected(ComponentName name) {
+	}
+
+
+	public void onServiceConnected(ComponentName className, IBinder service) {
+		bednetService = IBEDnetService.Stub.asInterface(service);
+		if (bednetService == null)
+		try {
+			applicationIdentifierHash = bednetService.registerCallback(
+					callback, applicationIdentifier);
+		} catch (RemoteException e) {
+
+			e.printStackTrace();
+		}
+	}
+
+
+
+
+	private IBEDnetServiceCallback callback = new IBEDnetServiceCallback.Stub() {
+
+		/**
+		 * This is called by the remote service regularly to tell us about new
+		 * values. Note that IPC calls are dispatched through a thread pool
+		 * running in each process, so the code executing here will NOT be
+		 * running in our main thread like most other things -- so, to update
+		 * the UI, we need to use a Handler to hop over there.
+		 */
+
+		public long getApplicationIdentifierHash() throws RemoteException {
+			return applicationIdentifierHash;
+		}
+
+		public void update(String senderAddress, byte[] message)
+				throws RemoteException {
+		}
+
+		@Override
+		public void updateWithSendersApplicationIdentifierHash(
+				String senderAddress, long senderApplicationIdentifierHash,
+				byte[] message) throws RemoteException {
+
+		}
+	};
+}
